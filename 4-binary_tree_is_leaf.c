537af42e16aee3130cd13ea30ae78596e13c3249
pether20 <pether.te.z@hotmail.com>
2022-03-23 13:16:39 -0500

    Add script function checks if a noded is a leaf

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,9 +1,10 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_delete - it is a function to create node
- * @tree: pointer to the parent node
+ * binary_tree_is_leaf - it is a function to create node
+ * @node: pointer to the parent node
  *
+ * Return: Value 1 is leaf o 0 if not
  */
 
 int binary_tree_is_leaf(const binary_tree_t *node)
