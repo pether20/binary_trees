85b4be1fcdc94de981b7179f4448f4c3ec147578
pether20 <pether.te.z@hotmail.com>
2022-03-23 13:26:29 -0500

    Add script function checks if a noded is a root

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_root - it is a function to create node
+ * @node: pointer to the parent node
+ *
+ * Return: Value 1 is leaf o 0 if not
+ */
+
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	if (node != NULL)
+	{
+		if (node->right != NULL && node->left != NULL)
+		{
+			return (1);
+		}
+		else
+			return (0);
+	}
+	return (0);
+}
