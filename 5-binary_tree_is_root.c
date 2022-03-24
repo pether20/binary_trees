73c7475145032891b1f6c56463222d1f81300a92
pether20 <pether.te.z@hotmail.com>
2022-03-24 14:25:54 -0500

    Add new check if node is root

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -9,14 +9,10 @@
 
 int binary_tree_is_root(const binary_tree_t *node)
 {
-
-	if (node)
+	if (node != NULL)
 	{
-		if (node->right != NULL && node->left != NULL)
-		{
+		if (node->parent == NULL)
 			return (1);
-		}
-		return (0);
 	}
 	return (0);
 }
