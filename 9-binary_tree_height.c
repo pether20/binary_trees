d44af42f931eec0c841f3e1fdec35c1f00e11ee2
pether20 <pether.te.z@hotmail.com>
2022-03-24 13:14:03 -0500

    Add script function  that measures theheight of a binary tree

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -14,9 +14,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (tree != NULL)
 	{
 		if (tree->left == NULL && tree->right == NULL)
-		{
 			return (0);
-		}
 
 		left_height = binary_tree_height(tree->left);
 		right_height = binary_tree_height(tree->right);
