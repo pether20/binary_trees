1e33657cde3b048156f851604d8f263309585acf
pether20 <pether.te.z@hotmail.com>
2022-03-24 14:37:07 -0500

    Add script function  that measures the size of a node

--- binary_trees.h
+++ binary_trees.h
@@ -38,5 +38,6 @@ size_t binary_tree_height(const binary_tree_t *tree);
 int max(int num1, int num2);
 
 size_t binary_tree_depth(const binary_tree_t *tree);
+size_t binary_tree_size(const binary_tree_t *tree);
 
 #endif /* _BINARY_TREES_H */
