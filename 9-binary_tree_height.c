81ae46049750a4583acd8dcda812686a82fb166a
pether20 <pether.te.z@hotmail.com>
2022-03-24 13:09:43 -0500

    Add script function  that measures theheight of a binary tree

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,46 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - it is a function to create node
+ * @tree: pointer to the parent node
+ *
+ *
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	int left_height, right_height;
+
+	if (tree != NULL)
+	{
+		if (tree->left == NULL && tree->right == NULL)
+		{
+			return (0);
+		}
+
+		left_height = binary_tree_height(tree->left);
+		right_height = binary_tree_height(tree->right);
+
+		return (max(left_height, right_height) + 1);
+	}
+	return (0);
+}
+
+/**
+ * max - function max value
+ *
+ * @num1: value
+ * @num2: value
+ * Return: int
+ */
+
+int max(int num1, int num2)
+{
+	int result;
+
+	if (num1 > num2)
+		result = num1;
+	else
+		result = num2;
+	return (result);
+}
--- binary_trees.h
+++ binary_trees.h
@@ -2,6 +2,7 @@
 #define _BINARY_TREES_H
 
 #include <stdlib.h>
+#include <stdio.h>
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -32,5 +33,8 @@ int binary_tree_is_root(const binary_tree_t *node);
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
+size_t binary_tree_height(const binary_tree_t *tree);
+
+int max(int num1, int num2);
 
 #endif /* _BINARY_TREES_H */
